f995fc2bad7b84f34f9a40e1c1ae86ecd2b58c1c
PhuNH <1079742+PhuNH@users.noreply.github.com>
2021-09-23 18:31:35 +0200

    Day 8 2017

---
+++ src/bin/2017_8.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = aoc::read_input();
+    let mut registers = HashMap::new();
+    let mut max = 0;
+    input.lines().for_each(|l| process_instruction(l, &mut registers, &mut max));
+    one(&registers);
+    two(max);
+}
+
+fn process_instruction(instr: &str, registers: &mut HashMap<String, i32>, max: &mut i32) {
+    // b inc 5 if a > 1
+    // 0 1   2 3  4 5 6
+    let words: Vec<_> = instr.split_whitespace().collect();
+    let cond_reg = *registers.entry(words[4].to_string()).or_insert(0);
+    let cond_val: i32 = words[6].parse().unwrap();
+    if match words[5] {
+        ">" => cond_reg > cond_val,
+        ">=" => cond_reg >= cond_val,
+        "<" => cond_reg < cond_val,
+        "<=" => cond_reg <= cond_val,
+        "==" => cond_reg == cond_val,
+        _ => cond_reg != cond_val,
+    } {
+        let op_val: i32 = words[2].parse().unwrap();
+        match words[1] {
+            "inc" => *registers.entry(words[0].to_string()).or_insert(0) += op_val,
+            _ => *registers.entry(words[0].to_string()).or_insert(0) -= op_val,
+        }
+        if *registers.get(words[0]).unwrap() > *max {
+            *max = *registers.get(words[0]).unwrap();
+        }
+    }
+}
+
+fn one(registers: &HashMap<String, i32>) {
+    println!("{}", registers.values().max().unwrap());
+}
+
+fn two(max: i32) {
+    println!("{}", max);
+}
